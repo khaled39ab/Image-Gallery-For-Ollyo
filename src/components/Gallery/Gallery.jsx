4eb2c3ab796e18e14e3216d05404b2849911c87a
khaled39ab <khaled39ab@gmail.com>
2023-11-04 18:47:56 +0600

    running......

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -9,6 +9,7 @@ const Gallery = ({ imageInfo }) => {
     const [checkedValue, setCheckedValue] = useState([]);
     const [itemCount, setItemCount] = useState(0);
     const [hovered, setHovered] = useState(false);
+    const [isSelected, setIsSelected] = useState(false);
 
     const handleChecked = (e) => {
         const { value, checked } = e.target;
@@ -17,10 +18,12 @@ const Gallery = ({ imageInfo }) => {
 
         if (checked) {
             setCheckedValue(pre => [...pre, getValue])
-            setItemCount(checkedValue.length + 1)
+            setItemCount(itemCount + 1);
+            setIsSelected(true);
         } else {
             setCheckedValue(pre => [...pre.filter(i => i !== getValue)])
-            setItemCount(checkedValue.length - 1)
+            setItemCount(itemCount- 1);
+            setIsSelected(false);
         }
     }
 
@@ -33,7 +36,7 @@ const Gallery = ({ imageInfo }) => {
 
 
     return (
-        <div className="m-5">
+        <div className="m-5 shadow-2xl p-5">
             <div className='my-5 py-2 px-5 border border-solid flex justify-between'>
                 <div className='mt-1'>
                     <h4 className='text-xl font-bold'>{itemCount} Files Selected</h4>
@@ -57,17 +60,18 @@ const Gallery = ({ imageInfo }) => {
 
                     images && images.map((item, index) => <div
                         key={item._id}
-                        className={`border-solid border-2 rounded-lg hover:bg-slate-300 imgCard ${index === 0 ? "col-span-2" : ""} ${index === 0 ? "row-span-2" : ""}`}
-                        onMouseEnter={() => setHovered(true)}
-                        onMouseLeave={() => setHovered(false)}
+                        className={`rounded-lg hover:bg-slate-300 imgCard ${isSelected ? 'border-solid border-2 border-indigo-400' : ''} ${index === 0 ? "md:col-span-2" : ""} ${index === 0 ? "md:row-span-2" : ""} `}
+                        // onMouseEnter={() => setHovered(true)}
+                        // onMouseLeave={() => setHovered(false)}
                     >
 
                         <input className={`scale-125 absolute ml-4 mt-4  imgCheck `} type="checkbox" value={item._id} onChange={handleChecked} />
                         <img className="rounded-lg" src={item.img} alt={`image${item._id}`} />
+                        <div className={`absolute hidden rounded-[10px] inset-0 bg-black bg-opacity-50 pointer-events-none z-10`}></div>
                     </div>)
 
                 }
-                <div className="flex flex-col gap-2 rounded-[10px] cursor-pointer border-2 border-dashed border-slate-500 justify-center items-center bg-slate-300">
+                <div className="flex flex-col gap-2 rounded-lg min-h-[220px] cursor-pointer border-2 border-dashed border-slate-500 justify-center items-center bg-slate-300">
                     <img className="w-12 h-12" src={addImg} alt="" />
                     <h4>Add Images</h4>
                 </div>
